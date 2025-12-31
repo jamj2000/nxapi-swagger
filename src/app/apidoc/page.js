2cc8fdfb2a4b265ce82832725ff4c7ce916e6766
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-12-31 13:31:04 +0100

    review

--- src/app/apidoc/page.js
+++ src/app/apidoc/page.js
@@ -1,10 +1,23 @@
-"use client";
+'use client'
 
 import { useEffect, useRef } from "react";
 import SwaggerUI from "swagger-ui-dist/swagger-ui-es-bundle";
 import "swagger-ui-dist/swagger-ui.css";
 
-export default function SwaggerDocs() {
+
+export default function page() {
+    return (
+        <div>
+            <div className="h-20 grid place-content-center rounded-lg bg-blue-700 text-white text-4xl">
+                API Doc
+            </div>
+            <SwaggerDocs />
+        </div>
+    );
+}
+
+
+function SwaggerDocs() {
     const uiRef = useRef(null);
 
     useEffect(() => {
